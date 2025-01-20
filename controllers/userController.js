32aecba461e92d866501d0efc48d35e8705e02e8
Deepak Maurya <138859870+deepakmaurya1378@users.noreply.github.com>
2025-01-21 04:44:18 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -80,7 +80,7 @@ const loginUser = async (req, res) => {
     res
       .cookie("token", token, {
       httpOnly: true,
-      secure: true, 
+      secure: req.secure || process.env.NODE_ENV === "production", 
       maxAge: 3600000, 
       sameSite: "strict",
       })
