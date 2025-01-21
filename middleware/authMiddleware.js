5b06f393f8371f5543967640a008248dfeec4595
Deepak Maurya <138859870+deepakmaurya1378@users.noreply.github.com>
2025-01-21 05:31:54 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const jwtSecret = process.env.jwtSecret;
 
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
