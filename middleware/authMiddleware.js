cb5620af2c9a13ac06cc90efc189bd66e0e3512b
Deepak Maurya <138859870+deepakmaurya1378@users.noreply.github.com>
2025-01-22 03:15:09 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,31 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User.js');
 const jwtSecret = process.env.jwtSecret;
 
-const authMiddleware = (req, res, next) => {
-  const token = req.cookies.token;
+const authMiddleware = async (req, res, next) => {
+  const token = req.cookies.token;  
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
-
+  
   try {
     const decoded = jwt.verify(token, jwtSecret);
-    req.user = decoded.user; // Attach user info to the request
+    const userId = decoded.user.id;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+    }
+
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;  
     next();
   } catch (err) {
+    console.error(err);
     res.status(401).json({ message: 'Invalid or expired token' });
   }
 };
